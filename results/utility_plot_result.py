425a68898df1ac926e8f3987423e2b0b990ad851
Michael Ostertag <mosterta@ucsd.edu>
2019-03-14 08:43:29 -0700

    update plot result and test_data_size

--- results/utility_plot_result.py
+++ results/utility_plot_result.py
@@ -1,31 +1,131 @@
 import os
-
-import pickle
+import pickle as pkl
 import matplotlib.pyplot as plt
+import numpy as np
+from itertools import compress
 
 
 directory = os.path.join(os.path.dirname(__file__), 'raw')
-filename_base = 'server_power_Data_Size_20190312191446_4188_models_final.pkl'
+#list_filename_base = ['server_power_Data_Size_20190313202112',
+#                      'server_power_Data_Size_20190313235700']
+#filename_target = 'results_server_power_data_size_3iter.pkl'
 
-with open(os.path.join(directory, filename_base), 'rb') as fid:
-    data = pickle.load(fid)
+list_filename_base = ['home_energy_Data_Size_20190312185618',
+                      'home_energy_Data_Size_20190312183943',
+                      'home_energy_Data_Size_20190312182359']
+filename_target = 'results_home_energy_data_size_initial.pkl'
 
+plot_type = 'Data_Size'
 
-plt.figure()
-plt.plot(data['Y_odq_predict'])
-plt.plot(data['Y_reservoir_predict'])
-plt.plot(data['Y_test'])
-plt.legend(('ODQ', 'Reservoir', 'Test'))
+if os.path.isfile(os.path.join(os.path.dirname(__file__), filename_target)):
+    print('Existing results file found. Loading...')
+    try:
+        with open(os.path.join(os.path.dirname(__file__), filename_target), 'rb') as fid:
+            data = pkl.load(fid)
+        print('  SUCCESS')
+    except:
+        print('  ERROR loading results file.')
 
-plt.figure()
-plt.plot(data['history_odq'].history['loss'])
-plt.plot(data['history_reservoir'].history['loss'])
-plt.legend(('ODQ', 'Reservoir'))
+else:
+    print('Existing results file not found. Generating...')
+    data = []
 
-plt.figure()
-plt.plot(data['history_odq'].history['val_loss'])
-plt.plot(data['history_reservoir'].history['val_loss'])
-plt.legend(('ODQ', 'Reservoir'))
+    for file in os.listdir(directory):
+        if not(file.lower().endswith('.pkl')):
+            continue
 
-plt.show()
-print('')
+        filename_match = [file.startswith(filename_base) for filename_base in list_filename_base]
+
+        if not(any(filename_match)):
+            continue
+
+        print('Loading data from {0}'.format(file))
+
+        try:
+            with open(os.path.join(directory, file), 'rb') as fid:
+                data_temp = pkl.load(fid)
+            print('  SUCCESS')
+        except:
+            print('  ERROR. Skipping.')
+
+        temp_filename_base = list(compress(list_filename_base, filename_match))[0]
+
+        list_filepieces = file.split(sep=temp_filename_base)[-1].split(sep='_')
+        n_datapoints = int(list_filepieces[1])
+        filetype = list_filepieces[-1]
+
+        if filetype == 'final.pkl':
+            try:
+                score_odq = np.sqrt(np.mean( (data_temp['Y_test'] - data_temp['Y_odq_predict'])**2 ))
+                score_res = np.sqrt(np.mean( (data_temp['Y_test'] - data_temp['Y_reservoir_predict'])**2 ))
+            except:
+                print('  ERROR. Missing expected entries.')
+                continue
+
+            # Look for entry in data that has same n_samples value
+            entry = list(filter(lambda data_entry: data_entry['n_datapoints'] == n_datapoints, data))
+            if entry:
+                entry[0]['score_odq'].append(score_odq)
+                entry[0]['score_res'].append(score_res)
+            else:
+                data.append({'n_datapoints': n_datapoints, 'score_odq': [score_odq], 'score_res': [score_res] })
+        elif filetype == 'full.pkl':
+            try:
+                score_full = np.sqrt(np.mean( (data_temp['Y_test'] - data_temp['Y_full_predict'])**2 ))
+            except:
+                print('  ERROR. Missing expected entries.')
+                continue
+
+            # Look for entry in data that relates to full dataset (n_datapoints = -1)
+            entry = list(filter(lambda data_entry: data_entry['n_datapoints'] == -1, data))
+            if entry:
+                entry[0]['score_full'].append(score_full)
+            else:
+                data.append({'n_datapoints': -1, 'score_full': [score_full]})
+
+        else:
+            print('  Unrecognized file type: {0}'.format(filetype))
+            continue
+
+    # Save results to remove need to regenerate
+    with open(os.path.join(os.path.dirname(__file__), filename_target), 'wb') as fid:
+        pkl.dump(data, fid)
+
+# Generate plots for Data Size
+if plot_type == 'Data_Size':
+    list_n          = np.array([])
+    list_odq_mean   = np.array([])
+    list_odq_std    = np.array([])
+    list_res_mean   = np.array([])
+    list_res_std    = np.array([])
+
+    for data_entry in data:
+        if int(data_entry['n_datapoints']) > 0:
+            list_n = np.append(list_n, int(data_entry['n_datapoints']))
+            list_odq_mean = np.append(list_odq_mean, np.mean(data_entry['score_odq']))
+            list_odq_std  = np.append(list_odq_std,  np.std(data_entry['score_odq']))
+            list_res_mean = np.append(list_res_mean, np.mean(data_entry['score_res']))
+            list_res_std  = np.append(list_res_std,  np.std(data_entry['score_res']))
+        else:
+            list_full_mean = np.mean(data_entry['score_full'])
+            list_full_std  = np.std(data_entry['score_full'])
+
+    ind_sorted = np.argsort(list_n)
+    list_n = list_n[ind_sorted]
+    list_odq_mean = list_odq_mean[ind_sorted]
+    list_odq_std  = list_odq_std[ind_sorted]
+    list_res_mean = list_res_mean[ind_sorted]
+    list_res_std  = list_res_std[ind_sorted]
+
+    plt.figure()
+    plt.rc('font', family='Liberation Serif', size=14)
+    plt.xscale('log')
+    plt.plot([np.min(list_n), np.max(list_n)], [list_full_mean, list_full_mean])
+    plt.errorbar(list_n, list_odq_mean, yerr=list_odq_std)
+    plt.errorbar(list_n, list_res_mean, yerr=list_res_std)
+    plt.legend(('Full', 'ODQ', 'Reservoir'))
+    plt.xlabel('Number of Samples Retained')
+    plt.ylabel('Error (MSE)')
+    plt.title('Appliance Energy Dataset')
+    plt.grid('on')
+    plt.show()
--- scripts/test_data_size.py
+++ scripts/test_data_size.py
@@ -71,16 +71,18 @@ def train_test_split(X, Y, pct_train=0.8):
 if __name__ == '__main__':
     FLAG_VERBOSE = False
     FLAG_PLOT = False
+    FLAG_SAVE_MODEL = False
     PLOT_DELAY = 0.0001
     DATASET = server_power
     ind_assess = [-1]
-    list_compression_ratio = np.append([1.2, 1.5], 2**(1 + np.arange(6)))[::-1]
+    list_compression_ratio = np.append([1.2, 1.5], 2**(1 + np.arange(8)))[::-1]
+    N_trials = 2
     TRAIN_VAL_RATIO = 0.8
     TRAIN_TEST_RATIO = 0.9 # Server power has test/train datasets pre-split due to tasks
 
     filename_base = datetime.now().strftime('{0}_Data_Size_%Y%m%d%H%M%S'.format(DATASET.__name__.split('.')[-1]))
 
-    np.random.seed(1235)
+    np.random.seed(1237)
 
     plt.ion()
 
@@ -154,129 +156,146 @@ if __name__ == '__main__':
     N_x = X_train.shape[1]
     N_y = Y_train.shape[1]
 
-    for compression_ratio in list_compression_ratio:
-        N_saved = int(N_datapoints // compression_ratio)
-        if DATASET is home_energy:
-            N_epochs = int(max(500, 50*compression_ratio))
+    for ind_loop in range(N_trials):
+        for compression_ratio in list_compression_ratio:
+            N_saved = int(N_datapoints // compression_ratio)
+            if DATASET is home_energy:
+                X_train, X_test, Y_train, Y_test = train_test_split(X, Y, pct_train=TRAIN_TEST_RATIO)
+                X_scaled = min_max_scaler_x.fit_transform(X_train)
+                Y_scaled = min_max_scaler_y.fit_transform(Y_train)
+                N_epochs = int(max(500, 50*compression_ratio))
+            else:
+                N_epochs = int(max(100, 20*compression_ratio))
+
+            print('\n\nCompression Ratio {0}: {1} -> {2}'.format(compression_ratio, N_datapoints, N_saved))
+            quantizer = OnlineDatasetQuantizer(num_datapoints_max=N_saved, num_input_features=N_x, num_target_features=N_y,
+                                               w_x_columns=w_x, w_y_columns=w_y)
+            reservoir_sampler = ReservoirSampler(num_datapoints_max=N_saved, num_input_features=N_x, num_target_features=N_y)
+
+            # Create machine learning models for each evaluation step
+            if DATASET is server_power:
+                model_odq = generate_model_server_power()
+                model_reservoir = generate_model_server_power()
+                model_full = generate_model_server_power()
+            elif DATASET is home_energy:
+                model_odq = generate_model_home_energy()
+                model_reservoir = generate_model_home_energy()
+                model_full = generate_model_home_energy()
+
+            for ind, X_new, Y_new in zip(range(N_datapoints), X_scaled, Y_scaled):
+                quantizer.add_point(X_new, Y_new)
+                reservoir_sampler.add_point(X_new, Y_new)
+                if (ind in ind_assess):
+                    print('\nind {0}: Generate model from ODQ-reduced Data'.format(ind))
+                    X_temp, Y_temp = quantizer.get_dataset()
+                    w_temp = quantizer.get_sample_weights()
+                    history_odq = model_odq.fit(X_temp, Y_temp, batch_size=32, epochs=N_epochs, sample_weight=w_temp, verbose=0,
+                                                validation_split=(1 - TRAIN_VAL_RATIO))
+                    score_odq = model_odq.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
+
+                    Y_odq_predict = min_max_scaler_y.inverse_transform(model_odq.predict(min_max_scaler_x.transform(X_test)))
+
+                    print('    RMSE: {0}'.format(np.sqrt(np.mean((Y_odq_predict - Y_test)**2))))
+
+                    print('\nind {0}: Generate model from Reservoir-reduced Data'.format(ind))
+                    X_temp, Y_temp = reservoir_sampler.get_dataset()
+                    history_reservoir = model_reservoir.fit(X_temp, Y_temp, batch_size=32, epochs=N_epochs, verbose=0,
+                                                validation_split=(1 - TRAIN_VAL_RATIO))
+                    score_reservoir = model_reservoir.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
+
+                    Y_reservoir_predict = min_max_scaler_y.inverse_transform(model_reservoir.predict(min_max_scaler_x.transform(X_test)))
+
+                    print('    RMSE: {0}'.format(np.sqrt(np.mean((Y_reservoir_predict - Y_test)**2))))
+
+                    # Save model progress
+                    with open(os.path.join(os.path.dirname(__file__), '..', 'results', 'raw', filename_base + '_{1}_models{0:06d}_{2}_partial.pkl'.format(ind, N_saved, ind_loop)), 'wb') as fid:
+                        pkl.dump({'model_odq': model_odq, 'model_reservoir': model_reservoir, 'history_odq': history_odq,
+                                  'score_odq': score_odq, 'Y_odq_predict': Y_odq_predict, 'score_reservoir': score_reservoir,
+                                  'Y_reservoir_predict': Y_reservoir_predict}, fid)
+
+                    if FLAG_PLOT:
+                        quantizer.plot('ODQ Sample {0:5d}'.format(ind), ind_dims=[0,1], fig_num=1)
+                        quantizer.plot('ODQ Sample {0:5d}'.format(ind), ind_dims=[0,-3], fig_num=2)
+                        quantizer.plot('ODQ Sample {0:5d}'.format(ind), ind_dims=[1,-3], fig_num=3)
+
+
+            # Plot full dataset for comparison
+            if FLAG_PLOT:
+                plt.figure(4)
+                plt.title('Full Dataset (X0 and X1)')
+                plt.scatter(X[:, 0], X[:, 1], s=1)
+                plt.grid(True)
+
+                plt.figure(5)
+                plt.title('Full Dataset (X0 and Y0)')
+                plt.scatter(X[:, 0], Y[:, 0], s=1)
+                plt.grid(True)
+
+                plt.figure(6)
+                plt.title('Full Dataset (X1 and Y0)')
+                plt.scatter(X[:, 1], Y[:, 0], s=1)
+                plt.grid(True)
+                plt.draw()
+                plt.pause(1)
+
+            time_start = time.time()
+            print('\nGenerate final model from ODQ-reduced Data')
+            X_temp, Y_temp = quantizer.get_dataset()
+            w_temp = quantizer.get_sample_weights()
+            history_odq = model_odq.fit(X_temp, Y_temp, batch_size=128, epochs=N_epochs, sample_weight=w_temp, verbose=0,
+                                        validation_split=0.1)
+            score_odq = model_odq.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
+
+            Y_odq_predict = min_max_scaler_y.inverse_transform(model_odq.predict(min_max_scaler_x.transform(X_test)))
+
+            print('    RMSE: {0:0.2f}'.format(np.sqrt(np.mean((Y_odq_predict - Y_test)**2))))
+            print('    Time: {0:0.2f} s'.format(time.time() - time_start))
+
+            time_start = time.time()
+            print('\nGenerate final model from Reservoir-reduced Data')
+            X_temp, Y_temp = reservoir_sampler.get_dataset()
+            history_reservoir = model_reservoir.fit(X_temp, Y_temp, batch_size=128, epochs=N_epochs, verbose=0,
+                                                    validation_split=0.1)
+            score_reservoir = model_reservoir.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
+
+            Y_reservoir_predict = min_max_scaler_y.inverse_transform(model_reservoir.predict(min_max_scaler_x.transform(X_test)))
+
+            print('    RMSE: {0:0.2f}'.format(np.sqrt(np.mean((Y_reservoir_predict - Y_test)**2))))
+            print('    Time: {0:0.2f} s'.format(time.time() - time_start))
+            if FLAG_SAVE_MODEL:
+                with open(os.path.join(os.path.dirname(__file__), '..', 'results', 'raw', filename_base + '_{0}_models_{1}_final.pkl'.format(N_saved, ind_loop)), 'wb') as fid:
+                    pkl.dump({'quantizer': quantizer, 'reservoir_sampler': reservoir_sampler,
+                              'model_odq': model_odq, 'model_reservoir': model_reservoir, 'history_odq': history_odq,
+                              'score_odq': score_odq, 'Y_odq_predict': Y_odq_predict, 'history_reservoir': history_reservoir,
+                              'score_reservoir': score_reservoir, 'Y_reservoir_predict': Y_reservoir_predict,
+                              'Y_test': Y_test, 'X_test': X_test}, fid)
+            else:
+                with open(os.path.join(os.path.dirname(__file__), '..', 'results', 'raw', filename_base + '_{0}_results_{1}_final.pkl'.format(N_saved, ind_loop)), 'wb') as fid:
+                    pkl.dump({'quantizer': quantizer, 'reservoir_sampler': reservoir_sampler,
+                              'history_odq': history_odq, 'score_odq': score_odq, 'Y_odq_predict': Y_odq_predict,
+                              'history_reservoir': history_reservoir, 'score_reservoir': score_reservoir, 'Y_reservoir_predict': Y_reservoir_predict,
+                              'Y_test': Y_test, 'X_test': X_test}, fid)
+
+
+        # Save full model
+        time_start = time.time()
+        print('\nGenerate final model from full dataset')
+        history_full = model_full.fit(X_scaled, Y_scaled, batch_size=128, epochs=400, verbose=0,
+                                      validation_split=0.1)
+        score_full = model_full.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
+
+        Y_full_predict = min_max_scaler_y.inverse_transform(model_full.predict(min_max_scaler_x.transform(X_test)))
+
+        print('    RMSE: {0:0.2f}'.format(np.sqrt(np.mean((Y_full_predict - Y_test) ** 2))))
+        print('    Time: {0:0.2f} s'.format(time.time() - time_start))
+
+        if FLAG_SAVE_MODEL:
+            with open(os.path.join(os.path.dirname(__file__), '..', 'results', 'raw',
+                                   filename_base + '_{0}_model_{1}_full.pkl'.format(N_saved, ind_loop)), 'wb') as fid:
+                pkl.dump({'model_full': model_full, 'history_full':history_full, 'score_full': score_full,
+                          'Y_full_predict': Y_full_predict, 'Y_test': Y_test, 'X_test': X_test}, fid)
         else:
-            N_epochs = int(max(100, 20*compression_ratio))
-
-        print('\n\nCompression Ratio {0}: {1} -> {2}'.format(compression_ratio, N_datapoints, N_saved))
-        quantizer = OnlineDatasetQuantizer(num_datapoints_max=N_saved, num_input_features=N_x, num_target_features=N_y,
-                                           w_x_columns=w_x, w_y_columns=w_y)
-        reservoir_sampler = ReservoirSampler(num_datapoints_max=N_saved, num_input_features=N_x, num_target_features=N_y)
-
-        # Create machine learning models for each evaluation step
-        if DATASET is server_power:
-            model_odq = generate_model_server_power()
-            model_reservoir = generate_model_server_power()
-            model_full = generate_model_server_power()
-        elif DATASET is home_energy:
-            model_odq = generate_model_home_energy()
-            model_reservoir = generate_model_home_energy()
-            model_full = generate_model_home_energy()
-
-        for ind, X_new, Y_new in zip(range(N_datapoints), X_scaled, Y_scaled):
-            quantizer.add_point(X_new, Y_new)
-            reservoir_sampler.add_point(X_new, Y_new)
-            if (ind in ind_assess):
-                print('\nind {0}: Generate model from ODQ-reduced Data'.format(ind))
-                X_temp, Y_temp = quantizer.get_dataset()
-                w_temp = quantizer.get_sample_weights()
-                history_odq = model_odq.fit(X_temp, Y_temp, batch_size=32, epochs=N_epochs, sample_weight=w_temp, verbose=0,
-                                            validation_split=(1 - TRAIN_VAL_RATIO))
-                score_odq = model_odq.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
-
-                Y_odq_predict = min_max_scaler_y.inverse_transform(model_odq.predict(min_max_scaler_x.transform(X_test)))
-
-                print('    RMSE: {0}'.format(np.sqrt(np.mean((Y_odq_predict - Y_test)**2))))
-
-                print('\nind {0}: Generate model from Reservoir-reduced Data'.format(ind))
-                X_temp, Y_temp = reservoir_sampler.get_dataset()
-                history_reservoir = model_reservoir.fit(X_temp, Y_temp, batch_size=32, epochs=N_epochs, verbose=0,
-                                            validation_split=(1 - TRAIN_VAL_RATIO))
-                score_reservoir = model_reservoir.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
-
-                Y_reservoir_predict = min_max_scaler_y.inverse_transform(model_reservoir.predict(min_max_scaler_x.transform(X_test)))
-
-                print('    RMSE: {0}'.format(np.sqrt(np.mean((Y_reservoir_predict - Y_test)**2))))
-
-                # Save model progress
-                with open(os.path.join(os.path.dirname(__file__), '..', 'results', filename_base + '_{1}_models{0:06d}.pkl'.format(ind, N_saved)), 'wb') as fid:
-                    pkl.dump({'model_odq': model_odq, 'model_reservoir': model_reservoir, 'history_odq': history_odq,
-                              'score_odq': score_odq, 'Y_odq_predict': Y_odq_predict, 'score_reservoir': score_reservoir,
-                              'Y_reservoir_predict': Y_reservoir_predict}, fid)
-
-                if FLAG_PLOT:
-                    quantizer.plot('ODQ Sample {0:5d}'.format(ind), ind_dims=[0,1], fig_num=1)
-                    quantizer.plot('ODQ Sample {0:5d}'.format(ind), ind_dims=[0,-3], fig_num=2)
-                    quantizer.plot('ODQ Sample {0:5d}'.format(ind), ind_dims=[1,-3], fig_num=3)
-
-
-        # Plot full dataset for comparison
-        if FLAG_PLOT:
-            plt.figure(4)
-            plt.title('Full Dataset (X0 and X1)')
-            plt.scatter(X[:, 0], X[:, 1], s=1)
-            plt.grid(True)
-
-            plt.figure(5)
-            plt.title('Full Dataset (X0 and Y0)')
-            plt.scatter(X[:, 0], Y[:, 0], s=1)
-            plt.grid(True)
-
-            plt.figure(6)
-            plt.title('Full Dataset (X1 and Y0)')
-            plt.scatter(X[:, 1], Y[:, 0], s=1)
-            plt.grid(True)
-            plt.draw()
-            plt.pause(1)
-
-        time_start = time.time()
-        print('\nGenerate final model from ODQ-reduced Data')
-        X_temp, Y_temp = quantizer.get_dataset()
-        w_temp = quantizer.get_sample_weights()
-        history_odq = model_odq.fit(X_temp, Y_temp, batch_size=128, epochs=N_epochs, sample_weight=w_temp, verbose=0,
-                                    validation_split=0.1)
-        score_odq = model_odq.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
-
-        Y_odq_predict = min_max_scaler_y.inverse_transform(model_odq.predict(min_max_scaler_x.transform(X_test)))
-
-        print('    RMSE: {0:0.2f}'.format(np.sqrt(np.mean((Y_odq_predict - Y_test)**2))))
-        print('    Time: {0:0.2f} s'.format(time.time() - time_start))
-
-        time_start = time.time()
-        print('\nGenerate final model from Reservoir-reduced Data')
-        X_temp, Y_temp = reservoir_sampler.get_dataset()
-        history_reservoir = model_reservoir.fit(X_temp, Y_temp, batch_size=128, epochs=N_epochs, verbose=0,
-                                                validation_split=0.1)
-        score_reservoir = model_reservoir.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
-
-        Y_reservoir_predict = min_max_scaler_y.inverse_transform(model_reservoir.predict(min_max_scaler_x.transform(X_test)))
-
-        print('    RMSE: {0:0.2f}'.format(np.sqrt(np.mean((Y_reservoir_predict - Y_test)**2))))
-        print('    Time: {0:0.2f} s'.format(time.time() - time_start))
-
-        with open(os.path.join(os.path.dirname(__file__), '..', 'results', filename_base + '_{0}_models_final.pkl'.format(N_saved)), 'wb') as fid:
-            pkl.dump({'quantizer': quantizer, 'reservoir_sampler': reservoir_sampler,
-                      'model_odq': model_odq, 'model_reservoir': model_reservoir, 'history_odq': history_odq,
-                      'score_odq': score_odq, 'Y_odq_predict': Y_odq_predict, 'history_reservoir': history_reservoir,
-                      'score_reservoir': score_reservoir, 'Y_reservoir_predict': Y_reservoir_predict,
-                      'Y_test': Y_test, 'X_test': X_test}, fid)
-
-    # Save full model
-    time_start = time.time()
-    print('\nGenerate final model from full dataset')
-    history_full = model_full.fit(X_scaled, Y_scaled, batch_size=128, epochs=400, verbose=0,
-                                  validation_split=0.1)
-    score_full = model_full.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
-
-    Y_full_predict = min_max_scaler_y.inverse_transform(model_full.predict(min_max_scaler_x.transform(X_test)))
-
-    print('    RMSE: {0:0.2f}'.format(np.sqrt(np.mean((Y_full_predict - Y_test) ** 2))))
-    print('    Time: {0:0.2f} s'.format(time.time() - time_start))
-
-    with open(os.path.join(os.path.dirname(__file__), '..', 'results',
-                           filename_base + '_{0}_model_full.pkl'.format(N_saved)), 'wb') as fid:
-        pkl.dump({'model_full': model_full, 'history_full':history_full, 'score_full': score_full,
-                  'Y_full_predict': Y_full_predict, 'Y_test': Y_test, 'X_test': X_test}, fid)
+            with open(os.path.join(os.path.dirname(__file__), '..', 'results', 'raw',
+                                   filename_base + '_{0}_results_{1}_full.pkl'.format(N_saved, ind_loop)), 'wb') as fid:
+                pkl.dump({'history_full':history_full, 'score_full': score_full,
+                          'Y_full_predict': Y_full_predict, 'Y_test': Y_test, 'X_test': X_test}, fid)
