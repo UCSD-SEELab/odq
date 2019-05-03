e3ae4c91ba250976f8d45f6ddb29451e291a0ebd
saldoweesh <sara-aldoweesh@hotmail.com>
2019-05-03 14:10:57 -0700

    Implement (run_sq_nn_tests) method in generate_results.py

--- scripts/ml_models.py
+++ scripts/ml_models.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 from keras import Model
 from keras.layers import Input, Dense, Dropout, GaussianNoise
 from keras.optimizers import Adam, SGD
@@ -59,6 +59,10 @@ def generate_model_home_energy(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b
 def generate_model_metasense(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd'):
     """
     Create neural network model
+    no. of features = 6 (N_x = 6)
+    ANN Maximum weights = 128000
+    no. of outputs = 2 (N_y = 2)
+    Defult = 41600
     """
     layer_input = Input(shape=(N_x,))  # Input features
     layer1 = GaussianNoise(stddev=std_noise)(layer_input)
@@ -79,19 +83,77 @@ def generate_model_metasense(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_c
         model_nn.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mse', 'mae'])
     return model_nn
 
-def generate_model_square(N_x, N_y, N_layer, N_weights, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd'):
+
+## Generate different ANN architectures
+def generate_model_square(N_x, N_y, N_layer, N_weights, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False,
+                          optimizer='sgd'):
     """
     description
 
-    N_layer:
-    N_weights:
+    N_layer: number of layers
+    N_weights: is the device total weight
+
     """
 
-    layer_input = Input(shape=(N_x,))
+    # Make the half of RAM size as max limit
+    N_weights = N_weights / 2
 
-    layer_out = Dense(N_y)(layer_input)
+    # N_weights = N_x*k + k*N_y + (N_layer-1)*K*K
+    # (N_layer-1 * k**2) + ((N_x + N_y) * k) - N_weights = 0
 
+    # with offset : N_weights = (N_x+1)*k + k*(N_y+1) + (N_layer-1)*(K+1)*(K)
+    # (N_x+1)*k + k*(N_y+1) + (N_layer-1)(K**2+k) = 0
+
+    a = N_layer - 1
+    b = N_x + N_y + 2 + (N_layer - 1)
+    c = - N_weights + (N_layer - 1)
+
+    # print("\n{}x**2 + {}x + {} = 0 has ".format(a, b, c), end="")
+
+    discriminant = b * b - 4 * a * c
+    if discriminant > 0:
+        rt = math.sqrt(discriminant)
+        root1 = (-b + rt) / (2 * a)
+        root2 = (-b - rt) / (2 * a)
+        # print("two real solutions: {0:0.4f} and {1:0.4f}".format(root1, root2))
+    elif discriminant == 0:
+        root1 = -b / (2 * a)
+        # print("one real solution: {0:0.4f}".format(root))
+    else:
+        root1 = -b / (2 * a)
+        imag = abs(math.sqrt(-discriminant) / (2 * a))
+        # print("two complex solutions: {0:0.4f} + {1:0.4f}i and {0:0.4f} - {1:0.4f}i".format(real, imag))
+
+    k = int(root1)  # Number of neurons
+
+    total_weights_test = N_x * k + k * N_y + (N_layer - 1) * k * k
+
+    print("Device RAM size = ", N_weights)
+    print("Total ANN weights using the positive value of root", total_weights_test)
+    print("Number of hidden layers = ", N_layer)
+
+    layers_list = []
+
+    layer_input = Input(shape=(N_x,))  # Input features
+    layers_list.append(layer_input)
+
+    for i in range(1, N_layer + 1):
+        if (i == 1):
+            layer = GaussianNoise(stddev=std_noise)(layers_list[i - 1])
+            layer = Dense(k, activation='relu')(layers_list[i - 1])
+            layer = Dropout(0.5)(layer)
+            layers_list.append(layer)
+
+        else:
+            layer = Dense(k, activation='relu')(layers_list[i - 1])
+            layer = Dropout(0.5)(layer)
+            layers_list.append(layer)
+
+    layer_out = Dense(N_y)(layers_list[N_layer])
     model_nn = Model(inputs=layer_input, outputs=layer_out)
+
+    print(model_nn.summary())
+
     if optimizer == 'sgd':
         optimizer = SGD(lr=lr, decay=decay)
     elif optimizer == 'adam':
@@ -102,6 +164,8 @@ def generate_model_square(N_x, N_y, N_layer, N_weights, std_noise=0.01, lr=0.001
     else:
         model_nn.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mse', 'mae'])
     return model_nn
+
+
 
 def train_test_split(X, Y, pct_train=0.8, weights=None):
     """
