973d8b926d42c83c63c22ab8411faaa61dcd597b
Felwah <falhamed@kacst.edu.sa>
2019-05-22 12:52:54 +0300

    Completed code in Generate results and implemented 2 loss functions

--- scripts/generate_results.py
+++ scripts/generate_results.py
@@ -4,7 +4,6 @@ import time
 import argparse
 from functools import partial
 from multiprocessing import Pool
-
 import numpy as np
 from keras.callbacks import EarlyStopping
 from keras import backend as K
@@ -233,6 +232,154 @@ def run_nn_tests(filename, dir_quant, dir_target, N_trials=3, b_cpu=True, list_l
                 K.clear_session()
 
 
+
+
+
+
+def run_nn_tests_with_lossF(filename, dir_quant, dir_target, N_trials=3, b_cpu=True, list_lr=[0.0001], list_std_noise=[0.001],
+                 TRAIN_VAL_RATIO = 0.8, b_usefreq=True, b_costmae=False , loss_fnc = 'mean_absolute_error'):
+    """
+    Generate results for neural network processing of the quantized dataset
+    """
+    FLAG_OVERWRITE = False
+
+    # Check that file is valid
+    if not (filename.lower().endswith('.pkl')):
+        return
+
+    filename_base = filename.replace('quantized.pkl', '')
+
+    print('\n\nLoading data from {0}'.format(filename))
+
+    try:
+        with open(os.path.join(dir_quant, dir_target, filename), 'rb') as fid:
+            data_temp = pkl.load(fid)
+        min_max_scaler_x = data_temp['min_max_scaler_x']
+        min_max_scaler_y = data_temp['min_max_scaler_y']
+      #  if 'quantizers' in data_temp:
+       #     list_quantizers = data_temp['quantizers']
+        #else:
+         #   list_quantizers = [{'quantizer':data_temp['quantizer'], 'w_x': [0], 'w_y': [0], 'w_imp': [0], 'w_type': 0}]
+       # reservoir_sampler = data_temp['reservoir_sampler']
+        X_train = data_temp['X_train']
+        Y_train = data_temp['Y_train']
+        X_test = data_temp['X_test']
+        Y_test = data_temp['Y_test']
+
+        filename_parts = filename.split('_')
+        if filename_parts[1] == 'data':
+            DATASET = eval('{0}'.format(filename_parts[0]))
+        else:
+            DATASET = eval('{0}_{1}'.format(filename_parts[0], filename_parts[1]))
+        compression_ratio = float(filename_parts[-3])
+        print('  SUCCESS')
+    except:
+        print('  ERROR. Skipping.')
+        return
+
+    # Process data
+    N_datapoints = X_train.shape[0]
+    N_x = X_train.shape[1]
+    N_y = Y_train.shape[1]
+
+    if DATASET is home_energy:
+        N_epochs = int(max(500, 50*compression_ratio))
+    else:
+        N_epochs = int(max(200, 30*compression_ratio))
+
+    print('\n\nCompression Ratio {0}'.format(compression_ratio))
+
+    for ind_loop in range(N_trials):
+        print('  Trial {0} of {1}:'.format(ind_loop + 1, N_trials))
+
+        for lr in list_lr:
+            for std_noise in list_std_noise:
+                config_tf_session(b_cpu)
+
+                print('  lr: {0}   std_noise: {1}:'.format(lr, std_noise))
+
+                if not(FLAG_OVERWRITE) and os.path.isfile(
+                        os.path.join(os.path.dirname(__file__), '..', 'results', 'raw',
+                                     dir_target,
+                                     filename_base + 'lr{1}_std{2}_f{3}_c{4}_results_trial{0}_reduced.pkl'.format(ind_loop, lr, std_noise, int(b_usefreq), int(b_costmae)))):
+                    print('  File already processed')
+                    continue
+
+                # Create an early stopping callback appropriate for the dataset size
+                cb_earlystopping = EarlyStopping(monitor='val_loss',
+                                                 patience=max([20, min([compression_ratio*lr/0.00005, 250])]),
+                                                 restore_best_weights=True)
+
+                # Create machine learning models for each evaluation step
+                list_model_odq = []
+                if DATASET is server_power:
+                    generate_model = generate_model_server_power
+                elif DATASET is home_energy:
+                    generate_model = generate_model_home_energy
+                elif DATASET is metasense:
+                    generate_model = generate_model_metasense
+
+                #for _ in list_quantizers:
+                 #   list_model_odq.append(generate_model(N_x, N_y, lr=lr, std_noise=std_noise, b_costmae=b_costmae, loss= loss_fnc))
+                model_reservoir = generate_model(N_x, N_y, lr=lr, std_noise=std_noise, b_costmae=b_costmae ,loss= loss_fnc)
+
+                # Perform training from reservoir data first, saving the validation set
+                time_start = time.time()
+              #  print('    Generating from Reservoir-reduced Data')
+               # X_temp, Y_temp = reservoir_sampler.get_dataset()
+
+                X_temp = min_max_scaler_x.transform(X_train)
+                Y_temp = min_max_scaler_y.transform(Y_train)
+
+                X_fit, X_val, Y_fit, Y_val = train_test_split(X_temp, Y_temp, pct_train=TRAIN_VAL_RATIO)
+
+                # Train the model on 10 epochs before checking for early stopping conditions to prevent premature return
+                history_temp = model_reservoir.fit(X_fit, Y_fit, batch_size=64, epochs=10, verbose=0,
+                                                   validation_data=(X_val, Y_val))
+                history_reservoir = history_temp.history
+                history_reservoir['epoch'] = history_temp.epoch
+                history_temp = model_reservoir.fit(X_fit, Y_fit, batch_size=64, epochs=N_epochs, verbose=0,
+                                                   validation_data=(X_val, Y_val), initial_epoch=10,
+                                                   callbacks=[cb_earlystopping])
+                history_reservoir['epoch'].extend(history_temp.epoch)
+                history_reservoir['val_loss'].extend(history_temp.history['val_loss'])
+                history_reservoir['val_mean_squared_error'].extend(history_temp.history['val_mean_squared_error'])
+                history_reservoir['val_mean_absolute_error'].extend(history_temp.history['val_mean_absolute_error'])
+                history_reservoir['loss'].extend(history_temp.history['loss'])
+                history_reservoir['mean_squared_error'].extend(history_temp.history['mean_squared_error'])
+                history_reservoir['mean_absolute_error'].extend(history_temp.history['mean_absolute_error'])
+
+                score_reservoir = model_reservoir.evaluate(min_max_scaler_x.transform(X_test), min_max_scaler_y.transform(Y_test), verbose=0)
+
+                Y_reservoir_predict = min_max_scaler_y.inverse_transform(model_reservoir.predict(min_max_scaler_x.transform(X_test)))
+
+                results_reservoir_rmse = np.sqrt(np.mean((Y_reservoir_predict - Y_test)**2, axis=0))
+
+                print('    RMSE: {0}'.format(np.array2string(results_reservoir_rmse, precision=2, suppress_small=True)))
+                print('    Time: {0:0.2f} s'.format(time.time() - time_start))
+
+                dict_out = {'history_reservoir': history_reservoir, 'score_reservoir': score_reservoir,
+                            'Y_reservoir_predict': Y_reservoir_predict, 'Y_test': Y_test, 'X_test': X_test,
+                            'N_datapoints': N_datapoints}
+
+
+                    # Save all results for subsequent processing
+                dir_target_full = os.path.join(os.path.dirname(__file__), '..', 'results', 'raw', dir_target)
+                if not os.path.exists(dir_target_full):
+                    os.mkdir(dir_target_full)
+
+                with open(os.path.join(dir_target_full,
+                                       filename_base + 'lr{1}_std{2}_f{3}_c{4}_results_trial{0}_reduced.pkl'.format(ind_loop, lr, std_noise, int(b_usefreq), int(b_costmae))), 'wb') as fid:
+                    pkl.dump(dict_out, fid)
+
+                # Reset Tensorflow session to prevent memory growth
+                K.clear_session()
+
+
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, help='Target directory of files.')
@@ -242,6 +389,7 @@ if __name__ == '__main__':
     parser.add_argument('--std', type=float, nargs='+', help='Noise std dev to use for NN training.', default=[0.001])
     parser.add_argument('--usefreq', action='store_true')
     parser.add_argument('--costmae', action='store_true')
+    parser.add_argument('--loss', type=str, help='loss function method, \'sigmoid\' or \'step\' ')
 
     args = parser.parse_args()
 
@@ -251,10 +399,16 @@ if __name__ == '__main__':
         dir_target = 'metasense_test_cov_max2_20190401'
 
     N_trials = args.N[0]
-
     dir_quant = os.path.join(os.path.dirname(__file__), '..', 'results', 'quantized')
 
-    p_run_nn_tests = partial(run_nn_tests, dir_quant=dir_quant, dir_target=dir_target, N_trials=N_trials,
+    if args.loss is not None:
+        loss_fnc = args.loss
+        p_run_nn_tests = partial(run_nn_tests_with_lossF, dir_quant=dir_quant, dir_target=dir_target, N_trials=N_trials,
+                             b_cpu=args.cpu, b_usefreq=args.usefreq, b_costmae=args.costmae,
+                             list_lr=args.lr, list_std_noise=args.std, TRAIN_VAL_RATIO=0.8, loss_fnc=loss_fnc)
+
+    else:
+         p_run_nn_tests = partial(run_nn_tests, dir_quant=dir_quant, dir_target=dir_target, N_trials=N_trials,
                              b_cpu=args.cpu, b_usefreq=args.usefreq, b_costmae=args.costmae,
                              list_lr=args.lr, list_std_noise=args.std, TRAIN_VAL_RATIO=0.8, )
 
--- scripts/ml_models.py
+++ scripts/ml_models.py
@@ -5,17 +5,23 @@ from keras.layers import Input, Dense, Dropout, GaussianNoise
 from keras.optimizers import Adam, SGD
 from keras import backend as K
 from functools import partial
+import tensorflow as tf
 
 
 # define sigmoid custom loss function
-def custom_loss_function (y_true, y_pred, m=1 , b=0) :
+def custom_loss_function_sig (y_true, y_pred, m=1 , b=0 ) :
 
-    alpha = K.exp(m*y_true +b) / (1 + K.exp(m*y_true + b))
+        alpha = K.exp(m*y_true +b) / (1 + K.exp(m*y_true + b))
+        return alpha*K.mean(K.square(y_pred - y_true))
 
-    return alpha*K.mean(K.square(y_pred - y_true))
+
+def custom_loss_function_step (y_true, y_pred , b=0 ) :
+
+        alpha = 0.5 * (tf.sign(y_true - b) + 1)
+        return alpha*K.mean(K.square(y_pred - y_true))
 
 
-def generate_model_server_power(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd'):
+def generate_model_server_power(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd', loss = 'mean_squared_error'):
     """
     Create neural network model for the server power dataset
     """
@@ -34,13 +40,22 @@ def generate_model_server_power(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, 
     elif optimizer == 'adam':
         optimizer = Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
+    loss_sig_b = 32.624
+    loss_sig_m = 0.125
+    loss_step_b = 1
+
+    if loss == 'sigmoid' :
+      loss = partial(custom_loss_function_sig, m=loss_sig_m, b=loss_sig_b)
+    elif loss == 'step':
+      loss = partial(custom_loss_function_step,  b=loss_step_b)
+
     if b_costmae:
-        model_nn.compile(optimizer=optimizer, loss='mean_absolute_error', metrics=['mse', 'mae'])
+        model_nn.compile(optimizer=optimizer, loss=loss, metrics=['mse', 'mae'])
     else:
-        model_nn.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mse', 'mae'])
+        model_nn.compile(optimizer=optimizer, loss=loss, metrics=['mse', 'mae'])
     return model_nn
 
-def generate_model_home_energy(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd'):
+def generate_model_home_energy(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd', loss = 'mean_squared_error'):
     """
     Create neural network model for the home energy dataset
     """
@@ -61,16 +76,23 @@ def generate_model_home_energy(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b
     elif optimizer == 'adam':
         optimizer = Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
-    # partial call sigmoid custom loss function
-    func_loss = partial(custom_loss_function, m=1, b=0)
+    loss_sig_b = 32.624
+    loss_sig_m = 0.125
+    loss_step_b = 1
+
+    if loss == 'sigmoid':
+        loss = partial(custom_loss_function_sig, m=loss_sig_m, b=loss_sig_b)
+    elif loss == 'step':
+        loss = partial(custom_loss_function_step, b=loss_step_b)
+
 
     if b_costmae:
-        model_nn.compile(optimizer=optimizer, loss=func_loss, metrics=['mse', 'mae'])
+        model_nn.compile(optimizer=optimizer, loss=loss, metrics=['mse', 'mae'])
     else:
-        model_nn.compile(optimizer=optimizer, loss=func_loss, metrics=['mse', 'mae'])
+        model_nn.compile(optimizer=optimizer, loss=loss, metrics=['mse', 'mae'])
     return model_nn
 
-def generate_model_metasense(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd'):
+def generate_model_metasense(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd', loss = 'mean_squared_error' ):
     """
     Create neural network model
     """
@@ -87,10 +109,19 @@ def generate_model_metasense(N_x, N_y, std_noise=0.01, lr=0.001, decay=1e-6, b_c
     elif optimizer == 'adam':
         optimizer = Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 
+    loss_sig_b = 32.624
+    loss_sig_m = 0.125
+    loss_step_b = 1
+
+    if loss == 'sigmoid':
+        loss = partial(custom_loss_function_sig, m=loss_sig_m, b=loss_sig_b)
+    elif loss == 'step':
+        loss = partial(custom_loss_function_step, b=loss_step_b)
+
     if b_costmae:
-        model_nn.compile(optimizer=optimizer, loss='mean_absolute_error', metrics=['mse', 'mae'])
+        model_nn.compile(optimizer=optimizer, loss=loss, metrics=['mse', 'mae'])
     else:
-        model_nn.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mse', 'mae'])
+        model_nn.compile(optimizer=optimizer, loss=loss, metrics=['mse', 'mae'])
     return model_nn
 
 def generate_model_square(N_x, N_y, N_layer, N_weights, std_noise=0.01, lr=0.001, decay=1e-6, b_costmae=False, optimizer='sgd'):
